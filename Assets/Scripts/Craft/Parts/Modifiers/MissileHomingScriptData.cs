4dbd0e4d8c7cda615346d789f562366b502d89ac
s5ehfr9 <51361374+s5ehfr9@users.noreply.github.com>
2021-02-28 09:21:11 +0800

    Functional MRAAM Type A

--- Assets/Scripts/Craft/Parts/Modifiers/MissileHomingScriptData.cs
+++ Assets/Scripts/Craft/Parts/Modifiers/MissileHomingScriptData.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 namespace Assets.Scripts.Craft.Parts.Modifiers
 {
     using System;
@@ -29,42 +28,7 @@ namespace Assets.Scripts.Craft.Parts.Modifiers
         public float torque;
         [DesignerPropertySlider(Label = "CoD Offset", MinValue = -1, MaxValue = 0)]
         public float centerOfDrag;
-        [DesignerPropertySpinner("Pure Pursuit", "Proportional", Label = "Guidance Method")]
+        [DesignerPropertySpinner("Pure Pursuit", "Proportional", "Unguided", Label = "Guidance Method")]
         public string guidanceMethod = "Pure Pursuit";
     }
-=======
-namespace Assets.Scripts.Craft.Parts.Modifiers
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Xml.Linq;
-    using ModApi.Craft.Parts;
-    using ModApi.Craft.Parts.Attributes;
-    using UnityEngine;
-
-    [Serializable]
-    [DesignerPartModifier("MissileHomingScript")]
-    [PartModifierTypeId("SimpleCombat.MissileHomingScript")]
-    public class MissileHomingScriptData : PartModifierData<MissileHomingScriptScript>
-    {
-        [DesignerPropertySlider(Label = "Missile Impulse", MinValue = 0, MaxValue = 2000)]
-        public float missileImpulse;
-        [DesignerPropertySlider(Label = "Missile Burn Time", MinValue = 0, MaxValue = 20)]
-        public float burnTime;
-        [DesignerPropertySlider(Label = "Target Acquire Range", MinValue = 0, MaxValue = 10000)]
-        public float targetRange;
-        [DesignerPropertySlider(Label = "Target Acquire Angle", MinValue = 0, MaxValue = 30)]
-        public float targetAngle;
-        [DesignerPropertySlider(Label = "Wing Area", MinValue = 0, MaxValue = 0.5f)]
-        public float wingArea;
-        [DesignerPropertySlider(Label = "Turning Torque", MinValue = 0, MaxValue = 1)]
-        public float torque;
-        [DesignerPropertySlider(Label = "CoD Offset", MinValue = -1, MaxValue = 0)]
-        public float centerOfDrag;
-        [DesignerPropertySpinner("Pure Pursuit", "Proportional", Label = "Guidance Method")]
-        public string guidanceMethod = "Pure Pursuit";
-    }
->>>>>>> a247e0d6c0f6a3fcc23dc94d307f08e72a065f06
 }
