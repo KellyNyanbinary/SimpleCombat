11dafc78a4258181a5b8d95362d505270cbc3ef9
s5ehfr9 <51361374+s5ehfr9@users.noreply.github.com>
2021-03-13 05:11:35 +0800

    Added comments to GunScript

--- Assets/Scripts/Craft/Parts/Modifiers/GunData.cs
+++ Assets/Scripts/Craft/Parts/Modifiers/GunData.cs
@@ -17,12 +17,14 @@ namespace Assets.Scripts.Craft.Parts.Modifiers
         [DesignerPropertySlider(Label = "Damage", MinValue = 1, MaxValue = 20)]
         public float damage = 1;
         [DesignerPropertySlider(Label = "Bullet Mass", MinValue = 0.01f, MaxValue = 0.2f)]
-        public float mass = 0.01f;
+        public float bulletMass = 0.01f;
         [DesignerPropertySlider(Label = "Muzzle Velocity", MinValue = 200, MaxValue = 1000)]
         public float muzzleVelocity = 200;
         [DesignerPropertySlider(Label = "Rate of Fire", MinValue = 20, MaxValue = 1000)]
         public float rateOfFire = 1;
         [DesignerPropertySlider(Label = "Bullet Dispersion", MinValue = 0, MaxValue = 1)]
         public float bulletDispersion = 0.1f;
+        [DesignerPropertySlider(Label = "Recoil Multiplier",MinValue =0,MaxValue =1.2f)]
+        public float recoil = 0;
     }
 }
--- Assets/Scripts/Craft/Parts/Modifiers/GunScript.cs
+++ Assets/Scripts/Craft/Parts/Modifiers/GunScript.cs
@@ -10,12 +10,10 @@ namespace Assets.Scripts.Craft.Parts.Modifiers
     using UnityEngine;
 
     public class GunScript : PartModifierScript<GunData>, IFlightFixedUpdate, IFlightStart, IFlightUpdate
-    {
+    {//Use of the particle buffer could still be optimized
         ParticleSystem gun;
         ParticleSystem.Particle[] bullets;
         Collider gunCollider;
-        bool firing;
-        float reloadProgress = 0;
         ParticleSystem.EmissionModule gunEmission;
 
         public void FlightStart(in FlightFrameData frame)
@@ -31,40 +29,39 @@ namespace Assets.Scripts.Craft.Parts.Modifiers
 
         public void FlightFixedUpdate(in FlightFrameData frame)
         {
-            if (PartScript.Data.Activated)
+            if (PartScript.Data.Activated)//control firing and rate of fire by changing the rate the particle system creates particles
                 gunEmission.rateOverTime = Data.rateOfFire;
             else gunEmission.rateOverTime = 0;
 
-            Debug.Log(gunEmission.rateOverTime.constant + "," + gun.isPlaying);
-            bullets = new ParticleSystem.Particle[gun.particleCount];
+            PartScript.BodyScript.RigidBody.AddForceAtPosition(-PartScript.Transform.forward * Data.rateOfFire * Data.bulletMass * Data.muzzleVelocity * Data.recoil, PartScript.Transform.position);//apply recoil
+
+            bullets = new ParticleSystem.Particle[gun.particleCount];//setup the buffer for handling particle data
             gun.GetParticles(bullets);
-            for (int i = 0; i < bullets.Length; i++)
+            for (int i = 0; i < bullets.Length; i++)//test for hits and update position for each bullet
             {
-                if (bullets[i].remainingLifetime > Time.deltaTime)
-                {
+                if (bullets[i].remainingLifetime > Time.deltaTime)//if bullet has not hit anything in previous fixed update
+                {//do a ray cast to check for collision
                     RaycastHit hit;
                     if (Physics.Raycast(bullets[i].position, bullets[i].velocity, out hit, bullets[i].velocity.magnitude * frame.DeltaTime))
                     {
-                        Debug.Log(bullets[i].velocity.magnitude * frame.DeltaTime);
-                        if (hit.collider != gunCollider)
+                        if (hit.collider != gunCollider)//ignores collider on the gun in case the bullet somehow goes behind the gun
                         {
-                            if (hit.collider.GetComponentInParent<PartScript>())
+                            if (hit.collider.GetComponentInParent<PartScript>())//if hits a part
                             {
                                 hit.collider.GetComponentInParent<PartScript>().TakeDamage(Data.damage, false);
-                                hit.rigidbody.AddForceAtPosition(bullets[i].velocity * Data.mass, hit.point, ForceMode.Impulse);
-                                Debug.Log(hit.collider.GetComponentInParent<PartScript>().name);
+                                hit.rigidbody.AddForceAtPosition(bullets[i].velocity * Data.bulletMass, hit.point, ForceMode.Impulse);
                             }
-                            bullets[i].remainingLifetime = Time.deltaTime;
+                            bullets[i].remainingLifetime = Time.deltaTime;//tell the particle system to destroy this particle on next update
                         }
                     }
-                    else bullets[i].position += bullets[i].velocity * frame.DeltaTime;
+                    else bullets[i].position += bullets[i].velocity * frame.DeltaTime;//moves the bullet forward
                 }
             }
-            gun.SetParticles(bullets);
+            gun.SetParticles(bullets);//apply changes to the particle system
         }
 
         public void FlightUpdate(in FlightFrameData frame)
-        {
+        {//move each particle back to previous position because it's updated in this script on fixed updates.
             bullets = new ParticleSystem.Particle[gun.particleCount];
             gun.GetParticles(bullets);
             for (int i = 0; i < bullets.Length; i++)
@@ -72,18 +69,6 @@ namespace Assets.Scripts.Craft.Parts.Modifiers
                 bullets[i].position -= bullets[i].velocity * frame.DeltaTime;
             }
             gun.SetParticles(bullets);
-        }
-
-        System.Collections.IEnumerator FireGun()//unused
-        {
-            firing = true;
-            while (PartScript.Data.Activated)
-            {
-                gun.Play();
-                yield return new WaitForSeconds(1 / Data.rateOfFire / Time.timeScale);
-            }
-            firing = false;
-            yield return null;
         }
     }
 }
